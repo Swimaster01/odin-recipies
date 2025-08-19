26f10b926c03cdc387b04fd2a8dfde4c764143d9
Swimaster01 <152829444+Swimaster01@users.noreply.github.com>
2025-08-19 13:17:40 +0800

    Updates on Practice

--- recipies/script.js
+++ recipies/script.js
@@ -5,3 +5,20 @@ let surname = "Doe";
 
 console.log(name);
 console.log(surname);
+
+// Re-assigning variables
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+//Adding 2 numbers
+
+console.log(5 + 10); // This will output 15
+
+let num1 = 23;
+let num2 = 24;
+
+console.log(num1 + num2); // This will output 47
